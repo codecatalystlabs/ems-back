f8b5d4d74a56842ac5988e567d167100cacf6f78
Patrck23 <ayebazibw68@gmail.com>
2026-03-12 17:44:56 +0300

    add user routes

--- internal/modules/users/infrastructure/http/routes.go
+++ internal/modules/users/infrastructure/http/routes.go
@@ -13,4 +13,17 @@ func RegisterRoutes(rg *gin.RouterGroup, h *Handler, rbacSvc *rbacapp.Service) {
 	rg.GET("/:id", rbacmiddleware.RequirePermission(rbacSvc, "users.read"), h.GetByID)
 	rg.PUT("/:id", rbacmiddleware.RequirePermission(rbacSvc, "users.update"), h.Update)
 	rg.DELETE("/:id", rbacmiddleware.RequirePermission(rbacSvc, "users.delete"), h.Delete)
+
+	rg.POST("/:id/change-password", h.ChangePassword)
+	rg.PATCH("/:id/profile", h.UpdateProfile)
+
+	rg.POST("/:id/roles", h.AssignRole)
+	rg.DELETE("/:id/roles/:roleId", h.RemoveRole)
+
+	rg.POST("/:id/assignments", h.AssignUser)
+	rg.PATCH("/assignments/:assignmentId", h.UpdateAssignment)
+
+	rg.POST("/:id/capabilities", h.AssignCapability)
+	rg.PATCH("/capabilities/:capabilityRecordId", h.UpdateCapability)
+	rg.GET("/:id/details", h.GetDetails)
 }
